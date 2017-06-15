3f7176babb610043576e9c7044588e4c48e1be08
Saff <saff.soobratty@gmail.com>
2017-06-15 19:32:10 +0100

    start of gulp-query-cache-buster

---
+++ gulp-query-cache-buster.js
@@ -0,0 +1,7 @@
+const LINK_REGEXP = new RegExp(`(?:href=|src=|url()['|"]([^s>"']+?)`); // eslint-disable-line quotes
+
+function gulpQueryCacheBuster() {
+
+}
+
+module.exports = gulpQueryCacheBuster;
